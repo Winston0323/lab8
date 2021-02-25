d00ce9e745ab8dc1a29d22163d939d997f0affc4
Winston0323 <liuzixi0323@gmail.com>
2021-02-25 00:54:14 -0800

    add map

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,13 @@
 function initMap() {
-	// add your code here
+	console.log("sucess");
+	L.mapquest.key = 'Qt5UqlNtRnEjUjrG0G7HFvIJnWhdmAAd';
+
+	// 'map' refers to a <div> element with the ID map
+	L.mapquest.map('map', {
+	  center: [37.7749, -122.4194],
+	  layers: L.mapquest.tileLayer('map'),
+	  zoom: 12,
+	  zoomControl:false
+	});// add your code here
+	L.marker([32.88, -117.236]).addTo(map);
 }
